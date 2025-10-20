f960986b0f81460b14a7cb55691dc67007e10c46
jeetparmar2810 <jeet.mobiledeveloper@gmail.com>
2025-10-20 14:06:38 +0530

    first commit

---
+++ sw.js
@@ -0,0 +1,37 @@
+
+const CACHE_NAME = 'jaimee-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((resp) => {
+      return resp || fetch(evt.request);
+    })
+  );
+});
